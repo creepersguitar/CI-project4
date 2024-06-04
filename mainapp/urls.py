82a69f67739b78475f4a99d33844fc03644a2f26
George Small <gsrr201@gmail.com>
2024-06-04 11:56:56 +0000

    corrected views import for urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from backgroundcode.views import test
-from . import views
+from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
