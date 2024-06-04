161ed25aaaffe6ca5937c8e192d48d7c49149ea6
George Small <gsrr201@gmail.com>
2024-06-04 12:00:02 +0000

    fixed internal server issue on urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from backgroundcode.views import test
-from .views import home
+from backgroundcode.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
