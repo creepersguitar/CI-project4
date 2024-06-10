1593444e645b4e66a0866239447c9e7ffee7e9a7
George Small <gsrr201@gmail.com>
2024-06-10 10:01:31 +0000

    added profile model

--- backgroundcode/models.py
+++ backgroundcode/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField()
+    location = models.CharField(max_length=100)
+    website = models.URLField()
 
 class Booking(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="bookings"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
     content = models.TextField()
     excerpt = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    
