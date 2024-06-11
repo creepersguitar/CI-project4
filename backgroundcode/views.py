1fad285bb60d1850b99e02b6fc4b1e0d80168620
George Small <gsrr201@gmail.com>
2024-06-11 08:53:09 +0000

    started making homepage for application

--- backgroundcode/views.py
+++ backgroundcode/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Booking
 #
 #
 #
-def test(request):
-    return HttpResponse("Hello, System!")
-def home(request):
-    return HttpResponse("Welcome to the Home Page")
+class booklist(generic.ListView):
+    model = Booking
