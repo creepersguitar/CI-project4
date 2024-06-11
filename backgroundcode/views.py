26bb1a400cc27c9283562238e556ffc81615d64d
George Small <gsrr201@gmail.com>
2024-06-11 09:16:14 +0000

    added changes to fix internal server error

--- backgroundcode/views.py
+++ backgroundcode/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Booking
-#
-#
-#
+
 class booklist(generic.ListView):
     model = Booking
+    template_name = 'booking_list.html'
