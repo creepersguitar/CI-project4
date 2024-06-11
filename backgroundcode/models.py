92e92994b230b772b4c8b1cb707bd17fc07a7056
George Small <gsrr201@gmail.com>
2024-06-11 08:03:17 +0000

    added meta class and booking str method

--- backgroundcode/models.py
+++ backgroundcode/models.py
@@ -17,3 +17,8 @@ class Booking(models.Model):
     excerpt = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"The title of this booking is {self.title}"
+        return f"The Name of this booking is {self.author}"
