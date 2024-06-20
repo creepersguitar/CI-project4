76130d323f0ca5c138c08fb369bf986a56c26dbe
George Small <gsrr201@gmail.com>
2024-06-20 08:42:17 +0000

    added booking_detail viewpoint

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.booklist.as_view(), name='home'),
+    path('<slug:slug>/', views.booking_detail, name='booking_detail'),
 ]
--- backgroundcode/views.py
+++ backgroundcode/views.py
@@ -1,7 +1,7 @@
 # backgroundcode/views.py
 
 import logging
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Booking
 
@@ -21,3 +21,25 @@ class booklist(generic.ListView):
         except Exception as e:
             logger.error("Error fetching bookings: %s", e)
             return []
+def booking_detail(request, slug):
+    """
+        Display an individual :model:`blog.Post`.
+
+        **Context**
+
+        ``post``
+            An instance of :model:`blog.Post`.
+
+        **Template:**
+
+        :template:`blog/post_detail.html`
+        """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
