13ea0d8d9cb12d131002586e8873d89b91584f71
George Small <gsrr201@gmail.com>
2024-06-27 09:34:53 +0000

    changed variable error in backgroundcode urls.py

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.booklist.as_view(), name='home'),
+    path('', views.base.as_view(), name='home'),
     path('bookings/', views.bookings, name='bookings'),
     path('create/', views.create_booking, name='bookings:create'),
 ]
