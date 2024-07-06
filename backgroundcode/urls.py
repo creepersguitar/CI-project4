1028f5a386d10a47b88e65915b7009f19dadc537
George Small <gsrr201@gmail.com>
2024-07-06 07:40:50 +0000

    changed part of urls.py in backgroundcode to fix internal error

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='home'),
-    path('bookings/', views.BookingListView.as_view(), name='bookings'),
+    path('bookings/', views.BookListView.as_view(), name='bookings'),
     path('create-booking/', views.create_booking, name='create_booking'),
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
 ]
