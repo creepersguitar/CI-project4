16aa6a5bdcb5710c686256c4aa1d595f607ca5fd
George Small <gsrr201@gmail.com>
2024-07-15 12:06:42 +0000

    removed path in backgroundcode urls.py

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('bookings/', views.BookListView.as_view(), name='bookings'),
     path('create-booking/', views.create_booking, name='create_booking'),
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
-    path('accounts/login/', views.LoginView.as_view(template_name='templates/login.html'), name='login'),
 ]
