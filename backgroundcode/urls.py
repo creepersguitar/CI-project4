566feebcade2e51990bf1ee93fc1b0b700e925d1
George Small <gsrr201@gmail.com>
2024-07-16 09:30:34 +0000

    changed name of bookings/ path

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='home'),
-    path('bookings/', views.BookListView.as_view(), name='bookings'),
+    path('bookings/', views.BookListView.as_view(), name='booking_list'),
     path('create-booking/', views.create_booking, name='create_booking'),
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
 ]
