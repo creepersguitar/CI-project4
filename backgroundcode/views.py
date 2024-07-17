2107981ba0c471764aadd62ee922fdccd09b0b10
George Small <gsrr201@gmail.com>
2024-07-17 15:56:05 +0000

    amended views.py file and base.html to show logout

--- backgroundcode/views.py
+++ backgroundcode/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from .forms import BookingForm
-from .models import Booking, CustomUser
-import logging
-
-logger = logging.getLogger(__name__)
-
-# View to display all bookings
-class BookListView(ListView):
-    model = Booking
-    template_name = 'index.html'
-    context_object_name = 'bookings'
-    paginate_by = 6
-
-    def get_queryset(self):
-        try:
-            logger.debug("Attempting to fetch bookings with status=1")
-            bookings = Booking.objects.filter(status=1)
-            logger.debug(f"Found {bookings.count()} bookings")
-            return bookings
-        except Exception as e:
-            logger.error("Error fetching bookings: %s", e)
-            return []
+from .models import Booking
+from django.http import JsonResponse
 
 @login_required
 def create_booking(request):
@@ -32,7 +12,14 @@ def create_booking(request):
             booking = form.save(commit=False)
             booking.author = request.user  # Set the author to the current user
             booking.save()
-            return redirect('booking_list')  # Redirect to a success page or the booking list
+
+            # Assuming you want to return a JSON response indicating success
+            return JsonResponse({'success': True})
+
+        # Handle form errors if not valid
+        errors = form.errors.as_json()
+        return JsonResponse({'success': False, 'errors': errors})
+
     else:
         form = BookingForm()
     return render(request, 'create_booking.html', {'form': form})
