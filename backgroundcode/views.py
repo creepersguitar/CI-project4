fcb215a1494768cce7f719dcdc784e152f8669da
George Small <gsrr201@gmail.com>
2024-07-17 16:03:01 +0000

    made sure all imports are in views.py

--- backgroundcode/views.py
+++ backgroundcode/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from .forms import BookingForm
 from .models import Booking
 from django.http import JsonResponse
