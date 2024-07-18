ed57a4faea7ac1f388af073765af3fa016d38506
George Small <gsrr201@gmail.com>
2024-07-18 08:28:33 +0000

    added more tests into test_forms.py

--- backgroundcode/test_forms.py
+++ backgroundcode/test_forms.py
@@ -29,3 +29,38 @@ class TestProfileForm(TestCase):
         print(form.errors)
         
         self.assertTrue(form.errors)  # Ensure that there are validation errors
+    def test_name_is_required(self):
+        """Test for the 'name' field"""
+        form = ProfileForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Name was not provided, but the form is valid"
+        )
+
+    def test_email_is_required(self):
+        """Test for the 'email' field"""
+        form = ProfileForm({
+            'name': 'Matt',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Email was not provided, but the form is valid"
+        )
+
+    def test_message_is_required(self):
+        """Test for the 'message' field"""
+        form = ProfileForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Message was not provided, but the form is valid"
+        )
