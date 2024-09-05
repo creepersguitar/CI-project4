4abfdf1678dd3fe1b63d842467121ed7a1ba99cb
George Small <gsrr201@gmail.com>
2024-09-05 09:38:02 +0000

    fixed urls.py to show correct file path

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('bookings/', views.BookListView.as_view(), name='booking_list'),
     path('create-booking/', views.create_booking, name='create_booking'),
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
-    path('booking-success/', views.booking_success, name='booking_success'),
+    path('booking-success/', views.booking_successful, name='booking_success'),
 ]
