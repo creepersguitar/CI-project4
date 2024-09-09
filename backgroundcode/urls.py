28cb0d4ee32380917c37f74b70775bfa1e1a3682
George Small <gsrr201@gmail.com>
2024-09-09 08:47:18 +0000

    checked spelling in url path for view booking

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create-booking/', views.create_booking, name='create_booking'),
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
     path('booking-success/', views.booking_successful, name='booking_success'),
-    path('view-booking/', views.view_booking, name='view_booking'),
+    path('view_booking/', views.view_booking, name='view_booking'),
 ]
