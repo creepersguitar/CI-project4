9aff6c1de8f5d190005d9d281f2e9055b1021dbd
George Small <gsrr201@gmail.com>
2024-09-09 09:30:45 +0000

    started work on the logout.html not loading properly

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
     path('booking-success/', views.booking_successful, name='booking_success'),
     path('view_booking/', views.view_booking, name='view_booking'),
+    path('accounts/', include('allauth.urls')),
 ]
