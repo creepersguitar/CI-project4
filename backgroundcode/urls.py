84865afdd7ca4951ea87084a0bd263fd1f80851c
George Small <gsrr201@gmail.com>
2024-09-09 09:49:30 +0000

    checked both urls.py files to allow logout.html to load

--- backgroundcode/urls.py
+++ backgroundcode/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,4 @@ urlpatterns = [
     path('booking/<int:booking_id>/', views.booking_detail, name='booking_detail'),
     path('booking-success/', views.booking_successful, name='booking_success'),
     path('view_booking/', views.view_booking, name='view_booking'),
-    path('accounts/', include('allauth.urls')),
 ]
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('backgroundcode.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
